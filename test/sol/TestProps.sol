570b01761b36c82b808d11f154a1d24f2b93c89e
Filip Soszyński <filip.soszynski@netguru.co>
2017-07-06 12:55:40 +0200

    Linting solidity files

--- test/sol/TestProps.sol
+++ test/sol/TestProps.sol
@@ -9,13 +9,13 @@ contract TestProps {
 
     function testUserBalance() {
         Props props = new Props();
-        props.register('test');
+        props.register("test");
         Assert.equal(props.userBalance(), 0, "User should have 0 ether to withdraw initially");
     }
 
     function testRegistrationMakesUserExists() {
         Props props = new Props();
-        var username = 'test name';
+        var username = "test name";
         Assert.isFalse(props.userExists(username), "User should NOT exist before registration");
         props.register(username);
         Assert.isTrue(props.userExists(username), "User should exists after registration");
@@ -23,7 +23,7 @@ contract TestProps {
 
     function testAccountExists() {
         Props props = new Props();
-        var username = 'first';
+        var username = "first";
         props.register(username);
         Assert.isTrue(props.userExists(username), "User should exists after registration");
         var account = props.account(username);
