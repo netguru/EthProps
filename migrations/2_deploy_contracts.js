f2a4e53f02f8a0c541090d190785f86b09f7b7a0
Filip Soszyński <filip.soszynski@netguru.co>
2017-07-11 14:23:35 +0200

    Bugfixes in migration file

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,7 +2,7 @@ let RandomSentence = artifacts.require('./RandomSentence.sol')
 let Props = artifacts.require('./Props.sol')
 
 module.exports = function (deployer) {
-  deployer.deploy(RandomSentence).then(function () {
-    deployer.deploy(Props, RandomSentence.address)
+  return deployer.deploy(RandomSentence).then(function () {
+    return deployer.deploy(Props, RandomSentence.address)
   })
 }
