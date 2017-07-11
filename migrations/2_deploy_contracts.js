4392c91a35b54a91a99f760b061f4590153dfc16
Filip Soszyński <filip.soszynski@netguru.co>
2017-07-11 12:45:21 +0200

    Add new contract to migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
-var Props = artifacts.require("./Props.sol");
+let RandomSentence = artifacts.require('./RandomSentence.sol')
+let Props = artifacts.require('./Props.sol')
 
-module.exports = function(deployer) {
-  deployer.deploy(Props);
-};
+module.exports = function (deployer) {
+  deployer.deploy(RandomSentence).then(function () {
+    deployer.deploy(Props, RandomSentence.address)
+  })
+}
