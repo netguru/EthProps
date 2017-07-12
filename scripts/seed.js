568383f2cf560c8e40531c3b02a7b49b7821e720
Filip Soszyński <filip.soszynski@netguru.co>
2017-07-12 09:47:57 +0200

    Initializing ether related things in seeds

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,21 +1,33 @@
-module.exports = function (done) {
+module.exports = function () {
   let Props = artifacts.require('./Props.sol')
+  let RandomSentence = artifacts.require('./RandomSentence.sol')
+
   let accounts = web3.eth.accounts
   let user1 = 'Joe'
   let user2 = 'Tony'
 
-  Props.deployed().then(function (instance) {
-    return Promise.all([
-      instance.register(user1),
-      instance.register(user2, { from: accounts[1] })
-    ]).then(function () {
-      return Promise.all([
-        instance.giveProps(user2, 'Great job!', { value: web3.toWei(3, 'ether') }),
-        instance.giveProps(user1, 'Thanks :)', { from: accounts[1], value: web3.toWei(5, 'ether') })
-      ])
-    }).then(function () {
-      console.log('Seeds done')
-      done()
+  let testAccount = '0x6f485C8BF6fc43eA212E93BBF8ce046C7f1cb475'
+  // Feel free to replace above with your test account (todo: move to app config)
+
+  web3.eth.sendTransaction({ from: accounts[0], to: testAccount, value: web3.toWei(1, 'ether') })
+
+  return RandomSentence.deployed()
+    .then(function (sentence) {
+      return sentence.sendTransaction({ from: accounts[0], value: web3.toWei(2, 'ether') })
     })
-  })
+    .then(function () {
+      return Props.deployed()
+    })
+    .then(function (props) {
+      return Promise.all([
+        props.register(user1),
+        props.register(user2, { from: accounts[1]  })
+      ])
+        .then(function () {
+          return props.giveProps(user2, 'This man is an expert in blockchain!', { value: web3.toWei(0.3, 'ether') })
+        })
+    })
+    .then(function () {
+      console.log("Seeds done")
+    })
 }
