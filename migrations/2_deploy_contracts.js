7853222ff66a81ddff257319ce93e5fa990aaab9
Filip Soszyński <filip.soszynski@netguru.co>
2017-07-12 09:47:31 +0200

    Update migration script to testable contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,33 @@
 let RandomSentence = artifacts.require('./RandomSentence.sol')
 let Props = artifacts.require('./Props.sol')
 
-module.exports = function (deployer) {
-  return deployer.deploy(RandomSentence).then(function () {
-    return deployer.deploy(Props, RandomSentence.address)
-  })
+let ethereumBridgeAddress = '0x6f485C8BF6fc43eA212E93BBF8ce046C7f1cb475'
+// Replace above with your bridge address (todo: move to some config file)
+
+module.exports = function (deployer, network) {
+  return deployer.deploy(RandomSentence)
+    .then(function () {
+      return RandomSentence.deployed()
+        .then(function (instance) {
+          return initializeRandomSentence(instance, network)
+        })
+    })
+    .then(function () {
+      return deployer.deploy(Props, RandomSentence.address)
+    })
+}
+
+function initializeRandomSentence (instance, network) {
+  return initializeOraclize(instance, network)
+    .then(function () {
+      return instance.update()
+    })
+}
+
+function initializeOraclize (instance, network) {
+  if (network === 'development') {
+    return instance.mockOraclize(ethereumBridgeAddress)
+  } else {
+    return Promise.resolve()
+  }
 }
