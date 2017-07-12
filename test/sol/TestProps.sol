331cd2d2d56650dbfd0195daae57b144e8805cf0
Filip Soszyński <filip.soszynski@netguru.co>
2017-07-12 10:25:21 +0200

    Add before hook to sol test

--- test/sol/TestProps.sol
+++ test/sol/TestProps.sol
@@ -2,31 +2,35 @@ pragma solidity ^0.4.11;
 
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
+import "../../contracts/RandomSentence.sol";
 import "../../contracts/Props.sol";
 
 
 contract TestProps {
 
+    Props props;
+
+    function beforeEach() {
+        props = new Props(new RandomSentence());
+    }
+
     function testUserBalance() {
-        Props props = new Props();
         props.register("test");
         Assert.equal(props.userBalance(), 0, "User should have 0 ether to withdraw initially");
     }
 
     function testRegistrationMakesUserExists() {
-        Props props = new Props();
-        var username = "test name";
+        string memory username = "test name";
         Assert.isFalse(props.userExists(username), "User should NOT exist before registration");
         props.register(username);
         Assert.isTrue(props.userExists(username), "User should exists after registration");
     }
 
     function testAccountExists() {
-        Props props = new Props();
-        var username = "first";
+        string memory username = "first";
         props.register(username);
         Assert.isTrue(props.userExists(username), "User should exists after registration");
-        var account = props.account(username);
+        address account = props.account(username);
         Assert.isTrue(props.accountExists(account), "User account should exist after registration");
     }
 }
